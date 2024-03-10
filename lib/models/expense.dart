4b9869af17f59d80c3c568dd54cb36680245360c
LiemTran <tranthanhliem.1993@hotmail.com>
2024-03-10 14:45:24 +0700

    First add

---
+++ lib/models/expense.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:uuid/uuid.dart';
+import 'package:intl/intl.dart';
+
+final formatter = DateFormat.yMd();
+
+const uuid = Uuid();
+
+enum Category { food, travel, leisure, work }
+
+const categoryIcons = {
+  Category.food: Icons.lunch_dining,
+  Category.travel: Icons.flight_takeoff,
+  Category.leisure: Icons.movie,
+  Category.work: Icons.work,
+};
+
+class Expense {
+  Expense({
+    required this.title,
+    required this.amount,
+    required this.date,
+    required this.category,
+  }) : id = uuid.v4();
+
+  final String id;
+  final String title;
+  final double amount;
+  final DateTime date;
+  final Category category;
+
+  String get formattedDate {
+    return formatter.format(date);
+  }
+}
+
+class ExpenseBucket {
+  const ExpenseBucket({
+    required this.category,
+    required this.expenses,
+  });
+
+  ExpenseBucket.forCategory(List<Expense> allExpenses, this.category)
+      : expenses = allExpenses
+            .where((expense) => expense.category == category)
+            .toList();
+
+  final Category category;
+  final List<Expense> expenses;
+
+  double get totalExpenses {
+    double sum = 0;
+
+    for (final expense in expenses) {
+      sum += expense.amount;
+    }
+
+    return sum;
+  }
+}
+
+//Initializer Lists
+//class Expense { /... }  : id = uuid.v4();
+
+//Format hiển thị thời gian
+//Sử dụng thư viện intl: https://pub.dev/packages/intl
+//Cài đặt: flutter pub add intl
+//final formatter = DateFormat.yMd();
+
+//Getter: tạo thuộc tính mới cho class dựa vào các thuộc tính đã có
+//https://flutterbyexample.com/lesson/getters-and-setters
+// String get formattedDate {
+//     return formatter.format(date);
+// }
